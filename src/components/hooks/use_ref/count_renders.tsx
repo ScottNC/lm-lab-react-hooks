4be5221a721cf0c34c3d3fe9d9616b6ecd66e7d0
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-07-17 10:31:02 +0100

    count renders shows frequency of key inputs

--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export const CountRenders = () => {
 	const [value, setValue] = useState('');
-	const count = 1;
+	const count = useRef<number>(0)
 
 	return (
 		<>
@@ -11,11 +11,14 @@ export const CountRenders = () => {
 			<input
 				value={value}
 				type='text'
-				onChange={(e) => setValue(e.target.value)}
+				onChange={(e) => {
+					setValue(e.target.value);
+					count.current++;
+				}}
 			/>
 
 			<p>{value}</p>
-			<p>I have rendered {count} times</p>
+			<p>I have rendered {count.current} times</p>
 		</>
 	);
 };
